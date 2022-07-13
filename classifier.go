bc6ef5585c33d20b4cdcf574ce9eabb3486e4cda
renthraysk <renthraysk@gmail.com>
2022-07-13 12:37:19 +0100

    Extensions in alphabetical order

--- classifier.go
+++ classifier.go
@@ -13,32 +13,32 @@ func (defaultClassifier) ContentTypeFromExt(ext string) string {
 	switch ext {
 	case ".avif":
 		return "image/avif"
-	case ".txt":
-		return "text/plain; charset=utf-8"
 	case ".css":
 		return "text/css; charset=utf-8"
-	case ".js", ".mjs":
-		return "text/javascript; charset=utf-8"
-	case ".htm", ".html":
-		return "text/html; charset=utf-8"
-	case ".json":
-		return "application/json; charset=utf-8"
-	case ".xml":
-		return "application/xml; charset=utf-8"
-	case ".svg":
-		return "image/svg+xml; charset=utf-8"
 	case ".gif":
 		return "image/gif"
-	case ".png":
-		return "image/png"
+	case ".htm", ".html":
+		return "text/html; charset=utf-8"
 	case ".jpg", ".jpeg":
 		return "image/jpeg"
+	case ".js", ".mjs":
+		return "text/javascript; charset=utf-8"
+	case ".json":
+		return "application/json; charset=utf-8"
 	case ".pdf":
 		return "application/pdf"
+	case ".png":
+		return "image/png"
+	case ".svg":
+		return "image/svg+xml; charset=utf-8"
+	case ".txt":
+		return "text/plain; charset=utf-8"
 	case ".wasm":
 		return "appliction/wasm"
 	case ".webp":
 		return "image/webp"
+	case ".xml":
+		return "application/xml; charset=utf-8"
 	}
 	return ""
 }
